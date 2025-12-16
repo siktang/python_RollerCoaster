44443a3f2223f3f8818ce30a68586d70232830d5
Denise Deng <siktang1221@gmail.com>
2025-12-16 12:00:01 -0500

    added functions

--- python_RollerCoaster/roller_coaster.py
+++ python_RollerCoaster/roller_coaster.py
@@ -1,16 +1,13 @@
 import pandas as pd
-import SymPy as sp
+import sympy as sp
+import sys
 
+x = sp.symbols('x')
 
 def load_csv_data(file_path: str) -> pd.DataFrame:
     """
-    Load roller coaster data from a CSV file into a pandas DataFrame.
-
-    Parameters:
-    file_path (str): The path to the CSV file containing roller coaster data.
-
-    Returns:
-    pd.DataFrame: A DataFrame containing the roller coaster data.
+    Returns a dataframe with the input csv data; 
+    returns empty dataframe if data is not available or error exists.
     """
     df = pd.read_csv(file_path)
     df_columns = set(df.columns)
@@ -33,11 +30,59 @@ def load_csv_data(file_path: str) -> pd.DataFrame:
         return pd.DataFrame()
 
 def create_segments(df: pd.DataFrame) -> list:
+    '''
+    Returns a list of segments in sympy expressions.
+    '''
+
     # loop through each row of df
     # df = load_csv_data(csvPath)
     # only run the following loop when df is truthy
-    for index, row in df.iterrows():
-        formula = row["formula"]
-        start_x = row["start_x"]
-        end_x = row["end_x"]
+    segments = []
+
+    for _, row in df.iterrows():
+        formula = sp.sympify(row["formula"])
+        start_x = sp.sympify(row["start_x"])
+        end_x = sp.sympify(row["end_x"])
+        segments.append((formula, start_x, end_x))
+    
+    return segments
+    
+
+# validation for each rule
+def validate_formula(formula: object) -> bool:
+    '''
+    Returns true if formula is valid (contains x or no variable) and false if it is not (contains other variables).
+    '''
+    input_variables = formula.free_symbols
+    return input_variables == { x } or input_variables == {}
+
+def validate_ending(start: float, end: float) -> bool: 
+    '''
+    Returns true if ending is larger than starting, and false otherwise.
+    ''' 
+    return end > start
+
+def validate_x_continuity(prev_end, current_start): 
+    '''
+    Returns true if the start point of the current segment matches the end point of previous segment.
+    '''
+    return prev_end == current_start
+
+def validate_y_continuity(prev_formula, current_formula, prev_end, current_start):
+    '''
+    Returns true if the values of formula1 and formula2 are the same at x.
+    '''
+    return prev_formula.subs(x, prev_end) == current_formula.subs(x, current_start)
+
+
+
+# then create a function to combine all the above validations to loop through segments
+def validate_segments(segments):
+    for item in segments: 
+        if validate_formula(item[0]) is False: 
+            sys.exit('Formula is not valid')
+        elif validate_ending(item[1], item[2]) is False: 
+            sys.exit('End point must be larger than start point.')
+        
+
     
